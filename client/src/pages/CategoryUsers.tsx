ca51c86c180f89f49e1816aaaa8bb037f5c73797
Lê Nhật Linh <lenhatlinh19042006@nh19042006gmail.42006gmailcom>
2025-10-08 07:12:57 +0700

    1

--- client/src/pages/CategoryUsers.tsx
+++ client/src/pages/CategoryUsers.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export default function CategoryUsers() {
-  return (
-    <div>CategoryUsers</div>
-  )
+  return <div>CategoryUsers</div>;
 }
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,12 +1,11 @@
-import { Button, Form, Input } from 'antd'
-import axios from 'axios';
-import type { FieldData } from 'rc-field-form/es/interface';
-import React, { useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
-import "../style/Sign.css"
-import { setUser } from '../features/userSlice';
-import { useDispatch } from 'react-redux';
-
+import { Button, Form, Input } from "antd";
+import axios from "axios";
+import type { FieldData } from "rc-field-form/es/interface";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Sign.css";
+import { setUser } from "../features/userSlice";
+import { useDispatch } from "react-redux";
 
 interface SignInForm {
   email: string;
@@ -21,9 +20,9 @@ export default function SignIn() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        await axios.get('http://localhost:3000/users');
+        await axios.get("http://localhost:3000/users");
       } catch (error) {
-        console.log('Lỗi khi lấy dữ liệu:', error);
+        console.log("Lỗi khi lấy dữ liệu:", error);
       }
     };
     fetchUsers();
@@ -38,19 +37,29 @@ export default function SignIn() {
     };
 
     if (!validateEmail(values.email)) {
-      errors.push({ name: ['email'], errors: ['Vui lòng nhập địa chỉ email hợp lệ'] });
+      errors.push({
+        name: ["email"],
+        errors: ["Vui lòng nhập địa chỉ email hợp lệ"],
+      });
     }
 
     try {
-      const response = await axios.get('http://localhost:3000/users');
+      const response = await axios.get("http://localhost:3000/users");
       const users = response.data;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const user = users.find((u: any) => u.email === values.email && u.password === values.password);
+      const user = users.find(
+        (u: any) => u.email === values.email && u.password === values.password
+      );
       // check signin
       if (!user) {
-        errors.push({ name: ['email'], errors: ['Email hoặc mật khẩu không đúng!'] });
-        errors.push({ name: ['password'], errors: ['Email hoặc mật khẩu không đúng!'] }); 
-      }else{
+        errors.push({
+          name: ["email"],
+          errors: ["Email hoặc mật khẩu không đúng!"],
+        });
+        errors.push({
+          name: ["password"],
+          errors: ["Email hoặc mật khẩu không đúng!"],
+        });
+      } else {
         dispatch(setUser(user));
       }
     } catch (error) {
@@ -61,39 +70,41 @@ export default function SignIn() {
       form.setFields(errors);
       return;
     }
-    navigate('/home', { replace: true });
+    navigate("/home", { replace: true });
   };
   return (
-    <div className='backgoundUser' style={{color:"white"}}>
-      <Form 
-          form={form} 
-          name="signin" 
-          onFinish={onFinish}
-          layout="vertical" 
-          className="signin-form" 
+    <div className="backgoundUser" style={{ color: "white" }}>
+      <Form
+        form={form}
+        name="signin"
+        onFinish={onFinish}
+        layout="vertical"
+        className="signin-form"
+      >
+        <h2>Đăng Nhập</h2>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[{ required: true, message: "Vui lòng nhập email!" }]}
         >
-          <h2>Đăng Nhập</h2> 
-          <Form.Item
-            name="email"
-            label="Email"
-            rules={[{ required: true, message: 'Vui lòng nhập email!' }]} 
-          >
-            <Input placeholder="Nhập email của bạn..." /> 
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="Mật Khẩu"
-            rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]} 
-          >
-            <Input.Password placeholder="Nhập mật khẩu..." /> 
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Đăng Nhập
-            </Button> 
-            <p>Bạn chưa có tài khoản? <a href="/signup">Đăng Ký</a></p> 
-          </Form.Item>
-        </Form>
+          <Input placeholder="Nhập email của bạn..." />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          label="Mật Khẩu"
+          rules={[{ required: true, message: "Vui lòng nhập mật khẩu!" }]}
+        >
+          <Input.Password placeholder="Nhập mật khẩu..." />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Đăng Nhập
+          </Button>
+          <p>
+            Bạn chưa có tài khoản? <a href="/signup">Đăng Ký</a>
+          </p>
+        </Form.Item>
+      </Form>
     </div>
-  )
+  );
 }
